07f7aa71c37aee868551fd8a6f9866d6c39d5128
blxe132435 <159596543+blxe132435@users.noreply.github.com>
2024-12-18 03:47:17 +0700

    Update server.js

--- node/server.js
+++ node/server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const app = expr();
 const port = 8080
 let db = ''
-
+app.use(cors())
 app.use(bodyParser.json()).use(bodyParser.text());
 const MySQL = async () => {
     db = await mysql.createConnection({
@@ -64,6 +64,51 @@ console.log(id);
     message: "updated successfully",
     old: oldData[0],
     new: newData[0]
+  });
+});
+
+app.get("/todolist", async (req, res) => {
+  let result = await db.query("SELECT * FROM todoList") 
+  res.json({
+    status: 200,
+    message: "This is the users route",
+    data: result[0]
+  });
+});
+app.post("/todolist", async (req, res) => {
+  let newTodo = req.body;
+  let result = await db.query("INSERT INTO todoList SET ?", newTodo);
+  let todo = await db.query("SELECT * FROM todoList WHERE id = ?", [result[0].insertId]);
+  res.json({
+    status: 200,
+    message: "This is the create user route",
+    data: user[0][0]
+  });
+});
+app.put("/todolist/:id", async (req, res) => {
+const id = req.params.id;
+console.log(id);
+  let dataUpdate = req.body;
+  const oldData = await db.query("SELECT * FROM todoList WHERE id = ?", [id]);
+  let result = await db.query("UPDATE todoList SET ? WHERE id = ?", [dataUpdate, id]);
+  const newData = await db.query("SELECT * FROM todoList WHERE id = ?", [id]);
+  res.json({
+    status: 200,
+    message: "updated successfully",
+    old: oldData[0],
+    new: newData[0]
+  });
+});
+app.delete("/todolist/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  let oldData = await db.query("SELECT * FROM users WHERE id = ?", [id]);
+  let results = await db.query("DELETE FROM users WHERE `id` = ?", [id]);
+  res.json({
+    status: 200, 
+    message: "Delete successfully",
+    data: oldData[0][0],
+    result: results
   });
 });
 app.listen(port, async () => {
